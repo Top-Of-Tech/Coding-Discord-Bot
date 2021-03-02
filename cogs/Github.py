3d5e92373b8b6177afeabbd2c83faa120bc672b5
Top-Of-Tech <67748521+Top-Of-Tech@users.noreply.github.com>
2021-03-02 08:26:59 -0600

    Update Github.py

--- cogs/Github.py
+++ cogs/Github.py
@@ -48,17 +48,17 @@ class Github(commands.Cog):
         embed = discord.Embed(title=user, url=user_object['url'], color=0x0066ff)
 
         if user_object['name'] != None:
-            embed.add_field(name="Name", value=user_object['name'], inline=True)
+            embed.add_field(name="Name", value=str(user_object['name']), inline=True)
         if user_object['company'] != None:
-            embed.add_field(name="Company", value=user_object['company'], inline=True)
+            embed.add_field(name="Company", value=str(user_object['company']), inline=True)
         if user_object['blog'] != None:
-            embed.add_field(name="Blog", value=user_object['blog'], inline=True)
+            embed.add_field(name="Blog", value=str(user_object['blog']), inline=True)
         if user_object['location'] != None:
-            embed.add_field(name="Location", value=user_object['location'], inline=True)
+            embed.add_field(name="Location", value=str(user_object['location']), inline=True)
         if user_object['twitter_username'] != None:
-            embed.add_field(name="Twitter Username", value=user_object['location'], inline=True)
+            embed.add_field(name="Twitter Username", value=str(user_object['location']), inline=True)
         if user_object['bio'] != None:
-            embed.add_field(name="Bio", value=user_object['bio'], inline=True)
+            embed.add_field(name="Bio", value=str(user_object['bio']), inline=True)
         
         embed.add_field(name="Repos", value=user_object['public_repos'], inline=True)
         embed.add_field(name="Gists", value=user_object['public_gists'], inline=True)
