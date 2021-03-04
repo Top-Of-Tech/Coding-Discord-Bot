df5515cd7b86f76ef4d01ff8892c335860e10871
class PythonAddict <72556571+classPythonAddike@users.noreply.github.com>
2021-03-04 07:39:29 +0530

    Added help commands for dictionary cog

--- cogs/Dictionary.py
+++ cogs/Dictionary.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from PyDictionary import PyDictionary
 
 class Dictionary(commands.Cog):
+    """Commands related to dictionaries! Use `.cs help Dictionary` to know more!"""
+    
     def __init__(self, client):
         self.client = client
 
@@ -11,6 +13,8 @@ class Dictionary(commands.Cog):
     # Gives you the definition(s) of the word
     @commands.command(aliases=["def"])
     async def definition(self, ctx, *, word):
+        """Get the meaning of any word, phrase or sentence. Usage: `.cs definition (word)`"""
+        
         meanings = PyDictionary(word.lower()).getMeanings()
 
         embed = discord.Embed(title=word.capitalize(), url=f"https://dictionary.com/browse/{word.lower()}", color=0x1479d2)
